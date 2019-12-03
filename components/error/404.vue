ddeba53d708c106ed37c1330cdf1495115f4aaed
Damien Robinson <shadow81627@gmail.com>
2019-12-04 06:19:02 +1000

    fix(404): add alt to background image

--- components/error/404.vue
+++ components/error/404.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <img
+      alt=""
       class="error__img img-fluid"
       src="https://res.cloudinary.com/pocketpasta/image/upload/c_fit,dpr_auto,g_auto,w_auto,f_auto,q_auto:low/v1575402067/anna-pascale-GoqEgxAgQw0-unsplash_qq2jte.jpg"
     />
