bf0e3e9c5035e8dea33f3d1f552f00156a624936
Damien Robinson <shadow81627@gmail.com>
2019-12-08 18:47:13 +1000

    feat(storyblok): add storyblok cms
    We can now update content with out deploying code.

---
+++ components/grid.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-editable="blok" class="grid">
+    <component
+      :is="blok.component"
+      v-for="content in blok.columns"
+      :key="content._uid"
+      :blok="content"
+    ></component>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { blok: { type: Object, required: true } },
+};
+</script>
---
+++ plugins/components.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import Page from '@/components/page.vue';
+import Teaser from '@/components/teaser.vue';
+import Grid from '@/components/grid.vue';
+import Feature from '@/components/feature.vue';
+
+Vue.component('page', Page);
+Vue.component('teaser', Teaser);
+Vue.component('grid', Grid);
+Vue.component('feature', Feature);
