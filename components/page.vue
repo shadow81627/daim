d5184efcfdda58797900a39c46aaa7e351887576
Damien Robinson <shadow81627@gmail.com>
2019-12-08 23:12:07 +1000

    feat(blog): add a blog section

--- components/page.vue
+++ components/page.vue
@@ -12,5 +12,9 @@
 <script>
 export default {
   props: { blok: { type: Object, required: true } },
+
+  head() {
+    return {};
+  },
 };
 </script>
---
+++ components/richtext.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div v-html="richtext"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { text: { type: String, default: null } },
+  computed: {
+    richtext() {
+      return this.text ? this.$storyapi.richTextResolver.render(this.text) : '';
+    },
+  },
+};
+</script>
