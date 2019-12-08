1096466d00cffe9571d020af157e126d5879f6b4
Damien Robinson <shadow81627@gmail.com>
2019-12-08 19:31:12 +1000

    feat(home): add content from storyblok

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,41 @@
 <template>
-  <div class="xcontainer">
-    <hero
-      :blok="{
-        headline: 'Daim',
-        desciption: 'Innovative Marketing Technology Automation',
-      }"
-    ></hero>
-  </div>
+  <section class="util__container">
+    <component
+      :is="story.content.component"
+      v-if="story.content.component"
+      :key="story.content._uid"
+      :blok="story.content"
+    ></component>
+  </section>
 </template>
 
 <script>
-import Hero from '~/components/hero.vue';
+// import storyblokLivePreview from '@/mixins/storyblokLivePreview';
 
 export default {
-  components: {
-    Hero,
+  // mixins: [storyblokLivePreview],
+  asyncData(context) {
+    // Check if we are in the editor mode
+    const version =
+      context.query._storyblok || context.isDev ? 'draft' : 'published';
+
+    // Load the JSON from the API
+    return context.app.$storyapi
+      .get('cdn/stories/home', {
+        version,
+      })
+      .then((res) => {
+        return res.data;
+      })
+      .catch((res) => {
+        context.error({
+          statusCode: res.response.status,
+          message: res.response.data,
+        });
+      });
+  },
+  data() {
+    return { story: { content: {} } };
   },
 };
 </script>
