3e633bbd1468dc3d666baad8de23fe07c691d8af
Damien Robinson <shadow81627@gmail.com>
2019-12-13 23:45:08 +1000

    refactor(hero): use parallax

--- components/hero.vue
+++ components/hero.vue
@@ -1,21 +1,19 @@
 <template>
-  <!-- Header -->
-  <header class="masthead">
-    <div class="container">
-      <div class="intro-text">
-        <h1 class="xintro-lead-in text-light text-capitalize text-shadow ">
-          {{ blok.headline }}
-        </h1>
-        <h2 class="xintro-heading text-light text-capitalize text-shadow">
-          {{ blok.desciption }}
-        </h2>
-        <!-- <a
-          class="btn btn-primary btn-xl text-uppercase js-scroll-trigger"
-          href="#services"
-          >Tell Me More</a
-        > -->
-      </div>
-    </div>
+  <header>
+    <v-parallax :src="require('~/assets/img/header-bg.jpg')">
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <h1
+            class="display-1 font-weight-thin mb-4 text-light text-shadow text-capitalize brand"
+          >
+            {{ blok.headline }}
+          </h1>
+          <h2 class="subheading text-light text-shadow">
+            {{ blok.desciption }}
+          </h2>
+        </v-col>
+      </v-row>
+    </v-parallax>
   </header>
 </template>
 
@@ -24,65 +22,3 @@ export default {
   props: { blok: { type: Object, required: true } },
 };
 </script>
-
-<style scoped>
-header.masthead {
-  text-align: center;
-  color: white;
-  background-image: url('~assets/img/header-bg.jpg');
-  background-repeat: no-repeat;
-  background-attachment: scroll;
-  background-position: center center;
-  background-size: cover;
-}
-
-header.masthead .intro-text {
-  padding-top: 150px;
-  padding-bottom: 100px;
-}
-
-header.masthead .intro-text .intro-lead-in {
-  font-size: 22px;
-  font-style: italic;
-  line-height: 22px;
-  margin-bottom: 25px;
-  font-family: 'Droid Serif', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji',
-    'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
-}
-
-header.masthead .intro-text .intro-heading {
-  font-size: 50px;
-  font-weight: 700;
-  line-height: 50px;
-  margin-bottom: 25px;
-  font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji',
-    'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
-}
-
-@media (min-width: 768px) {
-  header.masthead .intro-text {
-    padding-top: 300px;
-    padding-bottom: 200px;
-  }
-  header.masthead .intro-text .intro-lead-in {
-    font-size: 40px;
-    font-style: italic;
-    line-height: 40px;
-    margin-bottom: 25px;
-    font-family: 'Droid Serif', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-      Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji',
-      'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
-  }
-  header.masthead .intro-text .intro-heading {
-    font-size: 75px;
-    font-weight: 700;
-    line-height: 75px;
-    margin-bottom: 50px;
-    font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-      Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji',
-      'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
-  }
-}
-</style>
