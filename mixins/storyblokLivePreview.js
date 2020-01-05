303c142c056cf64458bc2fc762195e5e026cd8fd
Damien Robinson <damien.robinson@xcommedia.com.au>
2020-01-05 16:18:25 +1000

    chore(storyblok): update content on input

--- mixins/storyblokLivePreview.js
+++ mixins/storyblokLivePreview.js
@@ -1,12 +1,12 @@
 export default {
   mounted() {
-    this.$storybridge.on(['published', 'change'], (event) => {
-      if (!event.slugChanged) {
-        // Reload the page on save events (publish, save button or autosave)
-        this.$nuxt.$router.go({
-          path: this.$nuxt.$router.currentRoute,
-          force: true,
-        });
+    this.$storybridge.on(['input', 'published', 'change'], (event) => {
+      if (event.action === 'input') {
+        if (event.story.id === this.story.id) {
+          this.story.content = event.story.content;
+        }
+      } else {
+        window.location.reload();
       }
     });
   },
