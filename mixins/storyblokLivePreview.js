4a3b9b612ba9dd08fc85444365040c1f71076c20
Damien Robinson <shadow81627@gmail.com>
2020-02-02 18:12:12 +1000

    chore(storyblok): remove debug

--- mixins/storyblokLivePreview.js
+++ mixins/storyblokLivePreview.js
@@ -1,32 +1,5 @@
 export default {
   mounted() {
-    console.log(this.$storybridge);
-    this.$storybridge.load(
-      () => {
-        console.log(this.$storybridge);
-        if (this.$storybridge.proxy) {
-          console.log('storyblok');
-          this.$storybridge.proxy.pingEditor(function() {
-            if (this.$storybridge.proxy.inEditor) {
-              this.$storybridge.proxy.enterEditmode();
-            }
-          });
-        }
-      },
-      () => {
-        console.log('storyblok error');
-        console.log(this.$storybridge);
-        if (this.$storybridge.proxy) {
-          console.log('storyblok');
-          this.$storybridge.proxy.pingEditor(function() {
-            if (this.$storybridge.proxy.inEditor) {
-              this.$storybridge.proxy.enterEditmode();
-            }
-          });
-        }
-      },
-    );
-
     this.$storybridge.on(['input', 'published', 'change'], (event) => {
       if (event.action === 'input') {
         if (event.story.id === this.story.id) {
@@ -34,12 +7,6 @@ export default {
         }
       } else {
         window.location.reload();
-      }
-    });
-
-    this.$storybridge.pingEditor(function() {
-      if (this.$storybridge.inEditor) {
-        this.$storybridge.enterEditmode();
       }
     });
   },
