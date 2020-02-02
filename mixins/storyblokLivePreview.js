9ab451b3584816e130a09282e590db52bbfcdd99
Damien Robinson <damien.robinson@pocketpasta.com>
2020-02-02 17:27:10 +1000

    chore(storyblok): debug load event (#143)
    * chore(storyblok): in editor detector
    
    * chore(storyblok): debug load event

--- mixins/storyblokLivePreview.js
+++ mixins/storyblokLivePreview.js
@@ -1,5 +1,32 @@
 export default {
   mounted() {
+    console.log(this.$storybridge);
+    this.$storybridge.load(
+      () => {
+        console.log(this.$storybridge);
+        if (this.$storybridge.proxy) {
+          console.log('storyblok');
+          this.$storybridge.proxy.pingEditor(function() {
+            if (this.$storybridge.proxy.inEditor) {
+              this.$storybridge.proxy.enterEditmode();
+            }
+          });
+        }
+      },
+      () => {
+        console.log('storyblok error');
+        console.log(this.$storybridge);
+        if (this.$storybridge.proxy) {
+          console.log('storyblok');
+          this.$storybridge.proxy.pingEditor(function() {
+            if (this.$storybridge.proxy.inEditor) {
+              this.$storybridge.proxy.enterEditmode();
+            }
+          });
+        }
+      },
+    );
+
     this.$storybridge.on(['input', 'published', 'change'], (event) => {
       if (event.action === 'input') {
         if (event.story.id === this.story.id) {
