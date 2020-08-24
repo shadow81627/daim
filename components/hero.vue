c521a1ee77b9a8c3f4254861ff631eedd54ad959
Damien Robinson <damien.robinson@xcommedia.com.au>
2020-08-24 17:25:57 +1000

    refactor(hero): make gradient a prop

--- components/hero.vue
+++ components/hero.vue
@@ -2,36 +2,34 @@
   <v-container class="pa-0" style="max-width: 1785px;">
     <v-row no-gutters align="center" justify="center">
       <v-col cols="12" align-self="center">
-        <header>
+        <v-card :color="color" flat dark tile>
           <v-img
             :lazy-src="lazySrc"
             :src="src"
             :src-set="srcSet"
             :height="height"
             width="1785"
-            :style="{ backgroundColor }"
             sizes="(max-width: 1785px) 100vw, 1785px"
+            :gradient="gradient"
+            alt=""
           >
             <v-container
               class="fill-height align-items-end justify-start"
-              :style="{ backgroundColor: tint }"
               fluid
             >
               <v-row align="center" justify="center">
                 <v-col class="text-center" cols="12">
-                  <v-card color="transparent" flat dark>
-                    <h1 v-if="heading" class="mb-4 text-shadow">
-                      {{ heading }}
-                    </h1>
-                    <h2 v-if="subheading" class="subheading text-shadow">
-                      {{ subheading }}
-                    </h2>
-                  </v-card>
+                  <h1 v-if="heading" class="mb-4 text-shadow">
+                    {{ heading }}
+                  </h1>
+                  <h2 v-if="subheading" class="subheading text-shadow">
+                    {{ subheading }}
+                  </h2>
                 </v-col>
               </v-row>
             </v-container>
           </v-img>
-        </header>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -42,9 +40,15 @@ export default {
   props: {
     heading: { type: String, default: null },
     subheading: { type: String, default: null },
-    // image: { type: String, default: 'header-bg' },
-    tint: { type: String, default: 'rgba(0, 0, 0, 0.6)' },
+    gradient: {
+      type: String,
+      default: 'rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)',
+    },
     height: { type: [Number, String], default: 500 },
+    color: {
+      type: String,
+      default: require(`~/assets/img/header-bg.jpg?lqip-colors`)[0],
+    },
   },
   computed: {
     lazySrc() {
@@ -57,9 +61,6 @@ export default {
     srcSet() {
       return require(`~/assets/img/header-bg.jpg?resize&sizes[]=320&sizes[]=600&sizes[]=900&sizes[]=1785&sizes[]=4686&format=webp`)
         .srcSet;
-    },
-    backgroundColor() {
-      return require(`~/assets/img/header-bg.jpg?lqip-colors`)[0];
     },
   },
 };
