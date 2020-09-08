b40e7e34da017d6032f5b0d8c3d7ca3299e89141
Damien Robinson <damien.robinson@xcommedia.com.au>
2020-09-08 12:09:09 +1000

    feat: cover image

--- images.js
+++ images.js
@@ -1,4 +1,22 @@
+const { readdir } = require('fs').promises;
+const { resolve } = require('path');
 const sharp = require('sharp');
+
+/**
+ * https://stackoverflow.com/questions/5827612/node-js-fs-readdir-recursive-directory-search
+ * @param {String} dir
+ */
+async function* getFiles(dir) {
+  const dirents = await readdir(dir, { withFileTypes: true });
+  for (const dirent of dirents) {
+    const res = resolve(dir, dirent.name);
+    if (dirent.isDirectory()) {
+      yield* getFiles(res);
+    } else {
+      yield res;
+    }
+  }
+}
 
 const file = 'assets/img/header-bg.jpg';
 sharp(file)
@@ -6,3 +24,20 @@ sharp(file)
   .toFile(`assets/img/header-bg-1.jpg`, (err, info) => {
     console.log(err, info);
   });
+
+sharp(file)
+  .resize({ width: 1200, height: 600 })
+  .toFile(`static/cover.jpg`, (err, info) => {
+    console.log(err, info);
+  });
+
+(async () => {
+  const folder = 'assets/img/portfolio';
+  // get list of urls to crawl from content files
+  for await (const filename of getFiles(folder)) {
+    const buffer = await sharp(filename)
+      .resize({ width: 4686, height: 2636 })
+      .toBuffer();
+    sharp(buffer).toFile(filename);
+  }
+})();
