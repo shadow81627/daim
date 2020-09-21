d41b91a2b03433a8e826580249812c23a447e1d4
Damien Robinson <damien.robinson@pocketpasta.com>
2020-09-21 10:36:48 +1000

    ci: end to end testing (#615)
    * ci: end to end testing
    
    * fix(codecept): common js rather than module for node 12 support
    
    * fix(codecept): screenshots
    
    * test: tools and portfolio
    
    Co-authored-by: Damien Robinson <damien.robinson@xcommedia.com.au>

---
+++ test/e2e/home_test.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+
+const name = 'home';
+
+Feature(name);
+
+Scenario('Heading 1', async (I) => {
+  I.amOnPage(`/`);
+  I.see('DAMIEN ROBINSON', 'h1');
+
+  I.saveScreenshot(`${name}.png`);
+  await I.seeVisualDiff(`${name}.png`);
+});
