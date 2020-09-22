ee8a3b39c46c7bc278e1f30313701dbac4d8676a
Damien Robinson <damien.robinson@xcommedia.com.au>
2020-09-22 12:53:39 +1000

    chore(admin): open authoring

--- test/e2e/blog_test.js
+++ test/e2e/blog_test.js
@@ -4,13 +4,10 @@ const name = 'blog';
 
 Feature(name);
 
-Scenario('Heading 1', (I) => {
+Scenario('Heading 1', async (I) => {
   I.amOnPage(`/${name}`);
   I.see("DAMIEN ROBINSON'S BLOG", 'h1');
-});
 
-Scenario('Image Diff', async (I) => {
-  I.amOnPage(`/${name}`);
   I.saveScreenshot(`${name}.png`);
   await I.seeVisualDiff(`${name}.png`);
 });
