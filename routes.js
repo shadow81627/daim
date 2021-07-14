fc68b50141483a6c0593aa25f382f6361717d085
Damien Robinson <damien.robinson@pocketpasta.com>
2021-07-14 21:48:59 +1000

    chore(service-worker): move to static folder

--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ const { Router } = require('@layer0/core/router');
 const { nuxtRoutes } = require('@layer0/nuxt');
 
 module.exports = new Router()
-  .match('/service-worker.js', ({ serviceWorker }) => {
-    serviceWorker('.nuxt/dist/client/service-worker.js');
-  })
+  // .match('/service-worker.js', ({ serviceWorker }) => {
+  //   serviceWorker('.nuxt/dist/client/service-worker.js');
+  // })
   .use(nuxtRoutes);
