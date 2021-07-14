e364b5c0c52c753448f728effd40df12d5c313da
Damien Robinson <damien.robinson@pocketpasta.com>
2021-07-14 21:34:38 +1000

    fix(service-worker): remove clients claim

--- sw/service-worker.js
+++ sw/service-worker.js
@@ -1,9 +1,6 @@
-import { skipWaiting, clientsClaim } from 'workbox-core';
 import { precacheAndRoute } from 'workbox-precaching';
 import { Prefetcher } from '@layer0/prefetch/sw';
 
-skipWaiting();
-clientsClaim();
 precacheAndRoute(self.__WB_MANIFEST || []);
 
 new Prefetcher().route();
