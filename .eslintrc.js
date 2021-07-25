f3a8bd3a6ef111694bea2cb4b9b6afad0c54b792
Damien Robinson <damien.robinson@pocketpasta.com>
2021-07-25 21:30:19 +1000

    chore: typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,13 @@ module.exports = {
     browser: true,
     node: true,
   },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
   extends: [
-    '@nuxtjs',
+    '@nuxtjs/eslint-config-typescript',
     'prettier',
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
   ],
   plugins: ['prettier'],
-  // add your custom rules here
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
