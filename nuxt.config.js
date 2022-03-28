73fe6eb2549834ecd2feda043fc05b6f24132be7
Damien Robinson <damien.robinson@pocketpasta.com>
2022-03-28 11:16:25 +1000

    fix(matomo): add env

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,10 @@ const env = {
   APP_NAME:
     process.env.APP_NAME ||
     `${pkg.name.charAt(0).toUpperCase()}${pkg.name.slice(1)}`,
+
+  MATOMO_URL: process.env.MATOMO_URL ?? 'https://matomo.daim.dev/',
+  MATOMO_SITE_ID: process.env.MATOMO_SITE_ID ?? 2,
+  MATOMO_DEBUG: process.env.MATOMO_DEBUG ?? false,
 };
 
 const preconnectLinks = [];
@@ -166,8 +170,9 @@ export default {
   axios: {},
 
   matomo: {
-    matomoUrl: 'https://matomo.daim.dev',
-    siteId: 2,
+    matomoUrl: env.MATOMO_URL,
+    siteId: env.MATOMO_SITE_ID,
+    debug: env.MATOMO_DEBUG,
   },
 
   content: {
