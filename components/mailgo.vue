ca52890d9cbb242e7671ff199c5f4d3b248d6f4e
Damien Robinson <damien.robinson@pocketpasta.com>
2022-04-01 08:24:24 +1000

    feat(mailgo): override the reading direction

--- components/mailgo.vue
+++ components/mailgo.vue
@@ -1,7 +1,7 @@
 <template>
   <v-btn target="_blank" large text :href="href" rel="noopener">
     <font-awesome-icon v-if="icon" :icon="icon" fixed-width pull="left" />
-    <slot></slot>
+    <span class="reverse"><slot></slot></span>
   </v-btn>
 </template>
 
@@ -19,3 +19,15 @@ export default {
   },
 };
 </script>
+
+<style>
+/**
+  The reverse CSS will override the reading direction
+  and present the text to the user in the correct order.
+  https://stackoverflow.com/a/483239
+ */
+.reverse {
+  unicode-bidi: bidi-override;
+  direction: rtl;
+}
+</style>
