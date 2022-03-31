4de1a79f6551b3f62797a77f1cbee310fb868ead
Damien Robinson <damien.robinson@pocketpasta.com>
2022-04-01 08:45:40 +1000

    feat(mailgo): data attributes

--- components/mailgo.vue
+++ components/mailgo.vue
@@ -1,5 +1,14 @@
 <template>
-  <v-btn target="_blank" large text :href="href" rel="noopener">
+  <v-btn
+    target="_blank"
+    large
+    text
+    :href="url"
+    rel="noopener"
+    :data-address="address"
+    :data-domain="domain"
+    :data-tel="tel"
+  >
     <font-awesome-icon v-if="icon" :icon="icon" fixed-width pull="left" />
     <slot></slot>
   </v-btn>
@@ -11,6 +20,29 @@ export default {
   props: {
     href: { type: String, required: true },
     icon: { type: [String, Object], default: null },
+  },
+  computed: {
+    splits() {
+      return this.href.replace(/^mailto:/, '').split('@');
+    },
+    hasSplits() {
+      return this.splits.length > 1;
+    },
+    url() {
+      return this.hasSplits || this.isTel ? '#mailgo' : this.href;
+    },
+    address() {
+      return this.hasSplits ? this.splits[0] : undefined;
+    },
+    domain() {
+      return this.hasSplits ? this.splits[1] : undefined;
+    },
+    isTel() {
+      return this.href.startsWith('tel:');
+    },
+    tel() {
+      return this.isTel ? this.href.replace(/^tel:/, '') : undefined;
+    },
   },
   created() {
     if (process.client) {
