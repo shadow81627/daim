53205addf7ed3ee904f70b6fec4d0ea928ef3d44
Damien Robinson <damien.robinson@pocketpasta.com>
2022-04-29 08:32:39 +1000

    fix(utils price): minimumFractionDigits 0 when whole number

--- utils/price.js
+++ utils/price.js
@@ -8,8 +8,12 @@ export default function price(args) {
   const price = cost / (margin || 1);
   const nines =
     price === 0 || price % 1 !== 0 || margin === 0 ? price : price - 0.01;
+  const minimumFractionDigits = nines % 1 !== 0 ? 0 : 2;
+  const maximumSignificantDigits = 3;
   return nines.toLocaleString('en-US', {
     style: 'currency',
     currency,
+    minimumFractionDigits,
+    maximumSignificantDigits,
   });
 }
