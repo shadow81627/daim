e7181e827158a0b5004aceecd93a752f5c65de2b
Damien Robinson <damien.robinson@pocketpasta.com>
2022-05-07 11:25:15 +1000

    fix(fraction to decimal): return 0 if no fraction

--- utils/fraction-to-decimal.js
+++ utils/fraction-to-decimal.js
@@ -5,6 +5,9 @@
  * @returns {Number}
  */
 export default function fractionToDecimal(fraction) {
+  if (!fraction || !fraction.includes('/')) {
+    return 0;
+  }
   return fraction
     .split('/')
     .reduce((numerator, denominator, i) => numerator / (i ? denominator : 1));
