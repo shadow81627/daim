38f41cbebd10babd380d47f1d850cc2008493540
Damien Robinson <damien.robinson@pocketpasta.com>
2022-05-07 11:30:18 +1000

    feat(pages): add about

---
+++ pages/about.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <BlogHero
+      :title="heading"
+      :summary="description"
+      src="/img/banners/about.jpg"
+      :credit="{}"
+    ></BlogHero>
+    <About></About>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage',
+  data: () => ({
+    heading: 'About',
+    description: null,
+  }),
+  async fetch() {
+    const { description } = await this.$content('pages', 'about').fetch();
+    this.description = description;
+  },
+  fetchKey: 'page/about',
+  head() {
+    return {
+      title: this.heading,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.description,
+        },
+      ],
+    };
+  },
+};
+</script>
