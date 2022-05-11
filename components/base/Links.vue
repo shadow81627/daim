a39c2b7ce20f5454156fcb6ea8174b50637a44a0
Damien Robinson <damien.robinson@pocketpasta.com>
2022-05-11 15:20:44 +1000

    refactor: links component

---
+++ components/base/Links.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-list dense flat tile color="transparent">
+    <v-list-item
+      v-for="item in items"
+      :key="item.title"
+      :href="url(item.href)"
+      rel="noopener"
+      target="_blank"
+      :data-address="address(item.href)"
+      :data-domain="domain(item.href)"
+      :data-tel="tel(item.href)"
+      class="text-decoration-none"
+    >
+      <v-list-item-action style="font-size: 2em; width: 32px; height: 32px">
+        <BaseIcon :icon="item.icon"></BaseIcon>
+      </v-list-item-action>
+      <v-list-item-content>
+        <slot :item="item">
+          <v-list-item-title style="font-size: 16px; line-height: 1.4">{{
+            item.title
+          }}</v-list-item-title>
+          <v-list-item-subtitle v-if="item.subtitle">{{
+            item.subtitle
+          }}</v-list-item-subtitle>
+        </slot>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+import mailgo from 'mailgo';
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+  },
+  created() {
+    if (process.client) {
+      mailgo();
+    }
+  },
+  methods: {
+    stripped(href = '') {
+      return href.replace(/^mailto:/, '').replace(/^tel:/, '');
+    },
+    splits(href) {
+      return this.stripped(href).split('@');
+    },
+    hasSplits(href) {
+      return this.splits(href).length > 1;
+    },
+    url(href) {
+      return this.hasSplits(href) || this.isTel(href) ? '#mailgo' : href;
+    },
+    address(href) {
+      return this.hasSplits(href) ? this.splits(href)[0] : undefined;
+    },
+    domain(href) {
+      return this.hasSplits(href) ? this.splits(href)[1] : undefined;
+    },
+    isTel(href = '') {
+      return href.startsWith('tel:');
+    },
+    tel(href) {
+      return this.isTel(href) ? this.stripped(href) : undefined;
+    },
+  },
+};
+</script>
