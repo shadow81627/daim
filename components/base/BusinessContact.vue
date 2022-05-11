6a3bba828cda4ae707c8fa74c1553b3f6d3ab5cb
Damien Robinson <damien.robinson@pocketpasta.com>
2022-05-11 15:23:31 +1000

    refactor(business contact): data from content use links component

--- components/base/BusinessContact.vue
+++ components/base/BusinessContact.vue
@@ -5,61 +5,16 @@
         <slot></slot>
       </BaseInfoCard>
 
-      <template
-        v-for="({ icon, text, title: t, type, href, ...item }, i) in business"
-      >
-        <BaseAvatarCard
-          :key="i"
-          :xicon="$vuetify.breakpoint.mdAndUp ? icon : undefined"
-          :outlined="false"
-          :title="!dense ? t : undefined"
-          color="transparent"
-          :horizontal="$vuetify.breakpoint.mdAndUp"
-          dense
-          space="0"
-          style="overflow: hidden"
-        >
-          <LocationButton
-            v-if="type === 'location'"
-            v-bind="item.location"
-          ></LocationButton>
-          <mailgo v-else-if="type" :href="`${type}:${text}`"></mailgo>
-          <v-btn
-            v-else-if="href"
-            target="_blank"
-            large
-            text
-            :href="href"
-            rel="noopener"
-          >
-            <BaseIcon
-              v-if="icon && typeof icon === 'string'"
-              :icon="icon"
-              style="width: 14px; margin-right: 0.3em"
-              color="black"
-            ></BaseIcon>
-            <font-awesome-icon
-              v-else-if="icon"
-              :icon="icon"
-              fixed-width
-              pull="left"
-            />
-            {{ text || `Visit ${t}` }}
-            <BaseIcon
-              icon="mdi:open-in-new"
-              style="width: 14px; margin-left: 0.3em"
-              color="grey"
-            ></BaseIcon>
-          </v-btn>
-          <span v-else>{{ text }}</span>
-        </BaseAvatarCard>
-
-        <v-divider
-          v-if="i + 1 !== business.length && !dense"
-          :key="`divider-${i}`"
-          class="my-2"
-        />
-      </template>
+      <BaseLinks :items="items">
+        <template #default="{ item }">
+          <v-list-item-subtitle v-if="item.subtitle">{{
+            item.subtitle
+          }}</v-list-item-subtitle>
+          <v-list-item-title style="font-size: 16px; line-height: 1.4">{{
+            item.title
+          }}</v-list-item-title>
+        </template>
+      </BaseLinks>
     </div>
   </v-theme-provider>
 </template>
@@ -67,13 +22,7 @@
 <script>
 import { mdiOpenInNew } from '@mdi/js';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import Mailgo from '@/components/mailgo.vue';
-import LocationButton from '../LocationButton.vue';
 export default {
-  components: {
-    Mailgo,
-    LocationButton,
-  },
   props: {
     dark: Boolean,
     dense: { type: Boolean, default: true },
@@ -82,49 +31,22 @@ export default {
   data: () => ({
     faArrowRight,
     mdiOpenInNew,
-    business: [
-      {
-        icon: 'fa:phone',
-        title: 'Phone',
-        text: '+61-437-606-977',
-        type: 'tel',
-      },
-      {
-        icon: 'fa:envelope',
-        title: 'Email',
-        text: 'contact@daim.dev',
-        type: 'mailto',
-      },
-      {
-        type: 'location',
-        icon: 'fa:map-marker',
-        title: 'Location',
-        text: 'New Farm, Australia',
-        location: {
-          streetAddress: 'Salt Space 1/888 Brunswick Street',
-          city: 'New Farm',
-          countryCode: 'AU',
-          country: 'Australia',
-          postalCode: '4005',
-          region: 'Queensland',
-        },
-      },
-      {
-        icon: 'fa:linkedin',
-        title: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/daim-digital',
-      },
-      {
-        icon: 'fa:github',
-        title: 'GitHub',
-        href: 'https://github.com/daim-dev',
-      },
-      {
-        icon: 'mdi:car-clutch',
-        title: 'Clutch',
-        href: 'https://clutch.co/profile/daim-digital',
-      },
-    ],
+    items: [],
   }),
+  async fetch() {
+    const {
+      basics: { phone },
+    } = await this.$content('team', 'damien-robinson').fetch();
+
+    const socials = await this.$content('socials').fetch();
+    const contact = await this.$content('contact').fetch();
+
+    const data = {
+      items: [...contact, ...socials],
+      phone,
+    };
+    Object.assign(this.$data, data);
+  },
+  fetchKey: 'business-contact',
 };
 </script>
