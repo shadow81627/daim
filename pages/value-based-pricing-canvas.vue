4d0f4af90b2cbc7a0db96f9d11e204250117d6ff
Damien Robinson <damien.robinson@daim.dev>
2022-06-01 12:54:21 +1000

    feat(pages): add value based pricing canvas

---
+++ pages/value-based-pricing-canvas.vue
@@ -0,0 +1,288 @@
+<template>
+  <div class="container">
+    <h1>Value Based Pricing Canvas</h1>
+    <section>
+      <div>
+        <h2>1. The problem</h2>
+        <strong>What problem are you trying to solve?</strong>
+      </div>
+      <div>
+        <p>
+          I'm looking for a all in one back office solution that will help me
+          manage my business. As a Tech entrepreneur I want to automate the
+          boring bits of starting a new project.
+        </p>
+        <p>
+          When writing code there is a code principle called Don't Repeat
+          Yourself(DRY). As soon as I need to use something a second time I make
+          it reusable.
+        </p>
+        <p>
+          I have worked in digital agencies delivering public facing marketing
+          websites for over 5 years, across a range of industries. Every project
+          had a large amount of boring boilerplate that is always the same or
+          slightly different.
+        </p>
+        <p>
+          I want to have the developer experience of writing minimal code that
+          gives maximal results. I love open source and the community of sharing
+          and improving on common projects. I like to build on the shoulders of
+          giants.
+        </p>
+        <p>
+          I struggle to work with systems like wordpress since the code backing
+          them is really ugly, hard to logically organise and edit. Writing
+          custom code each time for a project is unsustainable because I now
+          have 10 slightly different version of the same code.
+        </p>
+      </div>
+    </section>
+    <section>
+      <div>
+        <h2>2. Pricing principles</h2>
+        <strong
+          >What principles do you want to adopt with your pricing
+          strategy?</strong
+        >
+      </div>
+      <div>
+        <p>
+          I want to make software affordable and sustainable. I need to balance
+          paying developers for their time and effort with the cost of the
+          product.
+        </p>
+        <p>
+          Open source developes write code for pure passion, but often get burnt
+          out. I want to make sure open source developers can keep working on
+          the projects that support my product.
+        </p>
+        <p>
+          Small business can't afford to pay much for software they are usually
+          bootstrapping with their own hard earned cash. Providing best in class
+          software at an affordable price to start ups will allow them to focus
+          on their business without having to worry about the technology.
+        </p>
+      </div>
+    </section>
+    <section>
+      <div>
+        <h2>3. Pricing behaviors</h2>
+        <strong>What behaviors do you want to encourage in customers?</strong>
+      </div>
+      <div>
+        <p>
+          I want customers to value good code. I want to encourage them to pay
+          open source developers. "Content is King, Code is Queen"
+        </p>
+      </div>
+    </section>
+    <section>
+      <div>
+        <h2>4. Pricing processes</h2>
+        <strong
+          >What processes do you want to adopt with your pricing
+          strategy?</strong
+        >
+      </div>
+      <div>
+        <p></p>
+      </div>
+    </section>
+    <section>
+      <div>
+        <h2>5. Fears</h2>
+        <strong
+          >What concerns might customers have about buying from you?</strong
+        >
+      </div>
+      <div>
+        <ul>
+          <li>Developer experience level</li>
+          <li>Cost</li>
+          <li>Deadlines</li>
+          <li>Software reliability</li>
+          <li>Vendor lock in</li>
+          <li>Security</li>
+          <li>Customer Service</li>
+          <li>Tech Support</li>
+        </ul>
+      </div>
+    </section>
+    <section>
+      <div>
+        <h2>6. Benefits</h2>
+        <strong>What benefits might customers get from buying from you?</strong>
+      </div>
+      <div>
+        <p>
+          I'm an experienced developer with a passion for making amazing
+          websites that deliver results that exceed expectations. I'm automating
+          the time I spend on building software so it cost less dev hours for
+          the client.
+        </p>
+        <p>
+          I working with innovating technology and bring the best in class
+          tooling.
+        </p>
+        <p>
+          I continually work on my communication skills to ensure working with
+          me is easy and everyone is on the same page. I take pride in not only
+          the quality of the code I write but also my written English.
+        </p>
+      </div>
+    </section>
+    <section>
+      <div>
+        <h2>7. Your appetite for change</h2>
+        <strong>Low, medium, or high?</strong>
+      </div>
+      <div>
+        <p>
+          I love change, thats why I'm a JavaScript developer. The iterative
+          process of improvement is a fun journey and can lead to quick
+          innovative solutions.
+        </p>
+      </div>
+    </section>
+    <section>
+      <div>
+        <h2>8. Your appetite for risk</h2>
+        <strong>Conservative, calculated, or aggressive?</strong>
+      </div>
+      <div>
+        <p>
+          I'm a cowboy and like to take on risk. I don't like to play within the
+          rules. I think outside the box and usually end up curb stomping the
+          box. I've been described as a knife without a handle by project
+          managers.
+        </p>
+        <p>
+          I also understand that project stakeholders have their own risk
+          tolerance and tailor my work to meet those expectations. I don't want
+          to disappoint anyone and do my best to communicate the risk level that
+          is acceptable for both of us.
+        </p>
+
+        <blockquote>
+          Break the rules, not the law, but break the rules. It is impossible to
+          be a maverick or a true original if you're too well behaved and don't
+          want to break the rules. You have to think outside the box.
+        </blockquote>
+        - Arnold Schwarzenegger
+      </div>
+    </section>
+    <section>
+      <div>
+        <h2>8. Your appetite for risk</h2>
+        <strong>Conservative, calculated, or aggressive?</strong>
+      </div>
+      <div>
+        <p></p>
+      </div>
+    </section>
+    <section>
+      <div>
+        <h2>9. Product value hierarchy</h2>
+        <strong>Features, benefits, and value</strong>
+      </div>
+      <div>
+        <p></p>
+      </div>
+    </section>
+    <section>
+      <div>
+        <h2>10. Your economic value</h2>
+        <strong
+          >Increase in revenue, reduction in costs, and minimization of
+          risk</strong
+        >
+      </div>
+      <div>
+        <p></p>
+      </div>
+    </section>
+    <section>
+      <div>
+        <h2>11. Your value proposition</h2>
+        <strong>Value provided, magnitude, and superiority</strong>
+      </div>
+      <div>
+        <p></p>
+      </div>
+    </section>
+    <section>
+      <div>
+        <h2>12. Your customers</h2>
+        <strong>Scrooge, Goldilocks, or James Bond?</strong>
+      </div>
+      <div>
+        <p></p>
+      </div>
+    </section>
+    <section>
+      <div>
+        <h2>13. Product characteristics</h2>
+        <strong>Necessity or discretionary? Unique or commodity?</strong>
+      </div>
+      <div>
+        <p>
+          Every organisation needs a website in the information age, it forms
+          the core of the business and is often the main channel for
+          communication.
+        </p>
+      </div>
+    </section>
+    <section>
+      <div>
+        <h2>14. Your competition</h2>
+        <strong>How do you compete on value and price?</strong>
+      </div>
+      <div>
+        <p>
+          There are many Content Management Systems and back office app software
+          out there. There are also plenty of freelancers and digital agencies.
+        </p>
+        <p>
+          There is still plenty of demand for the services of a developer in the
+          current market there are 2 jobs for every developer.
+        </p>
+        <p>
+          I want to compete on all aspects of the product and service I provide.
+          I'm very ambitious in what I want to provide. I want to generally
+          better than the rest. I want to learn from them, what works best and
+          what doesn't.
+        </p>
+        <p>
+          I want to build the product for myself to use and if other people like
+          it thats a bonus.
+        </p>
+      </div>
+    </section>
+    <section>
+      <div>
+        <h2>15. Behavioral economics hacks</h2>
+        <strong
+          >Status quo bias, loss aversion, time discounting, the bandwagon
+          effect, and more</strong
+        >
+      </div>
+      <div>
+        <p>
+          Re-platforming an existing website can be a hard task to convince
+          people to part with their money on. Other times people want to refresh
+          the brand and start fresh.
+        </p>
+        <p>
+          It would be nice to come in as a do it for you type digital agency
+          that can make a small business website quickly that just works for
+          them.
+        </p>
+        <p>
+          Once a website is built they are almost a set and forget and don't get
+          updated much. Making it easy for the client to update the website is a
+          good way to keep the website fresh and active.
+        </p>
+      </div>
+    </section>
+  </div>
+</template>
