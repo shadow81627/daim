3fdd963b324aefde864ced259056bf98563cbd08
Damien Robinson <damien.robinson@daim.dev>
2022-11-19 21:01:16 +1000

    chore(eslint): disable for config files

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const http = require('http');
 const { setHeadlessWhen } = require('@codeceptjs/configure');
 const handler = require('serve-handler');
--- release.config.js
+++ release.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const readFileSync = require('fs').readFileSync;
 const resolve = require(`path`).resolve;
 
