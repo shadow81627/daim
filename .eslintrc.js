7a1cd01eaf7ffc9d2fa0b96be6882bb4e867d5a5
Damien Robinson <damien.robinson@daim.dev>
2022-11-19 21:00:46 +1000

    chore(eslint): disable vue/multi-word-component-names

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/multi-word-component-names': 'off',
   },
 };
