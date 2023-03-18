9151f965fd485cdf87c7024e8f25f92388b5797c
Damien Robinson <damien.robinson@daim.dev>
2023-03-18 12:33:39 +1000

    fix(vuetify): add mdi svg icons

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -2,6 +2,7 @@
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
@@ -17,6 +18,13 @@ export default defineNuxtPlugin((nuxtApp) => {
         dark: {
           colors: { primary: '#1a1a1a', secondary: '#424242' },
         },
+      },
+    },
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi,
       },
     },
   });
