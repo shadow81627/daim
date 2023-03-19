33eac51601a90faf55547c03e4b2b6631ce367c6
Damien Robinson <damien.robinson@daim.dev>
2023-03-19 15:07:19 +1000

    fix(tooltip button component): remove v-on

--- components/base/TooltipButton.vue
+++ components/base/TooltipButton.vue
@@ -1,11 +1,10 @@
 <template>
   <v-tooltip location="top">
-    <template #activator="{ on, attrs }">
+    <template #activator="{ props }">
       <v-btn
         icon
-        v-bind="{ ...$attrs, ...attrs, ...btnAttrs }"
+        v-bind="{ ...$attrs, ...props, ...btnAttrs }"
         :class="buttonClass"
-        v-on="on"
       >
         <BaseIcon color="grey" :icon="_icon"></BaseIcon>
         <span class="d-sr-only-focusable">{{ tooltip }}</span>
