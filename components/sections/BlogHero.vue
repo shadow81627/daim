c74dceb87a3761f699d9d31944ed2a3843eeff85
Damien Robinson <damien.robinson@daim.dev>
2023-03-20 11:27:20 +1000

    fix(blog hero component): pass headings to hero

--- components/sections/BlogHero.vue
+++ components/sections/BlogHero.vue
@@ -1,5 +1,5 @@
 <template>
-  <Hero :src="src">
+  <Hero :src="src" v-bind="{ subheading: summary, heading: title }">
     <Breadcrumb absolute dark class="pa-4"></Breadcrumb>
     <v-row class="fill-height mx-0 pa-4" no-gutters>
       <v-col
