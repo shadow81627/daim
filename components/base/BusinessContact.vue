ee111b8e2528c59a43737224d1a265ca608c5b25
Damien Robinson <damien.robinson@daim.dev>
2023-03-20 10:43:43 +1000

    fix(business contact component): align heading left

--- components/base/BusinessContact.vue
+++ components/base/BusinessContact.vue
@@ -1,7 +1,7 @@
 <template>
   <v-theme-provider :dark="dark">
     <div>
-      <BaseInfoCard :title="title" color="primary" :level="2">
+      <BaseInfoCard :title="title" align="left" color="primary" :level="2">
         <slot></slot>
       </BaseInfoCard>
 
@@ -28,25 +28,31 @@ export default {
     dense: { type: Boolean, default: true },
     title: { type: String, default: null },
   },
+  async setup() {
+    const [{ data: phone }, { data: socials }, { data: contact }] =
+      await Promise.all([
+        useAsyncData(
+          'team-damien-robinson',
+          () => queryContent('team', 'damien-robinson').find(),
+          {
+            transform(data) {
+              const {
+                basics: { phone },
+              } = data;
+              return phone;
+            },
+          },
+        ),
+        useAsyncData('socials', () => queryContent('socials').find()),
+        useAsyncData('contact', () => queryContent('contact').find()),
+      ]);
+    const items = [...contact.value, ...socials.value];
+    return { items, phone };
+  },
   data: () => ({
     faArrowRight,
     mdiOpenInNew,
-    items: [],
   }),
-  async fetch() {
-    const {
-      basics: { phone },
-    } = await queryContent('team', 'damien-robinson').find();
-
-    const socials = await queryContent('socials').find();
-    const contact = await queryContent('contact').find();
-
-    const data = {
-      items: [...contact, ...socials],
-      phone,
-    };
-    Object.assign(this.$data, data);
-  },
   fetchKey: 'business-contact',
 };
 </script>
