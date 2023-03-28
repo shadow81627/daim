69e0547e8303dde67cc33a6ec5baf22503cbd145
Damien Robinson <damien.robinson@daim.dev>
2023-03-28 16:50:46 +1000

    fix(error page): add vite pwa manifest

--- error.vue
+++ error.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="flex items-center justify-center h-screen">
+    <VitePwaManifest />
+    <NuxtLoadingIndicator />
     <v-card id="message" max-width="360" class="text-left">
       <v-card-subtitle class="text--primary pb-0">
         <h2
