801b481fee0a7a34885f9d966b7af604db8c6d80
Damien Robinson <damien.robinson@daim.dev>
2023-04-07 13:18:36 +1000

    fix(breadcrumbs): tailwind styling

--- components/breadcrumb.vue
+++ components/breadcrumb.vue
@@ -1,12 +1,33 @@
 <template>
-  <v-toolbar flat height="65" color="transparent" :absolute="absolute">
-    <Breadcrumbs>
-      <template #breadcrumb="{ to, title }">
-        <NuxtLink :to="to">
-          {{ title }}
-        </NuxtLink>
+  <v-toolbar flat height="32" color="transparent" :absolute="absolute">
+    <ul
+      v-if="breadcrumbs.length > 1"
+      class="list-none flex"
+      itemprop="breadcrumb"
+      itemtype="https://schema.org/BreadcrumbList"
+      itemscope="itemscope"
+    >
+      <template v-for="(item, key) of breadcrumbs" :key="key">
+        <li itemprop="itemListElement">
+          <NuxtLink
+            :to="item.to"
+            class="text-decoration-none text-sm"
+            :class="{
+              'color-white': key !== breadcrumbs.length - 1,
+              'color-gray': key === breadcrumbs.length - 1,
+            }"
+          >
+            {{ item.title }}
+          </NuxtLink>
+        </li>
+        <li
+          v-if="key !== breadcrumbs.length - 1"
+          class="opacity-50 color-white px-3"
+        >
+          /
+        </li>
       </template>
-    </Breadcrumbs>
+    </ul>
   </v-toolbar>
 </template>
 
@@ -15,11 +36,18 @@ export default {
   props: {
     absolute: { type: Boolean, default: false },
   },
+  setup() {
+    const breadcrumbs = useBreadcrumbs();
+    const schemaBreadcrumbs = computed(() =>
+      breadcrumbs.value.map((breadcrumb) => breadcrumb.schema),
+    );
+
+    useSchemaOrg([
+      defineBreadcrumb({
+        itemListElement: schemaBreadcrumbs,
+      }),
+    ]);
+    return { breadcrumbs };
+  },
 };
 </script>
-
-<style>
-.v-application .v-breadcrumbs__item {
-  color: white;
-}
-</style>
