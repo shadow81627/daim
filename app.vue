3faeb7f1277a6152d129eef8701c0067fe6119c6
Damien Robinson <damien.robinson@daim.dev>
2024-11-17 00:00:17 +1000

    fix: apply sans font to html element

--- app.vue
+++ app.vue
@@ -16,6 +16,7 @@ const i18nHead = useLocaleHead({
 useHead({
   htmlAttrs: {
     lang: i18nHead.value.htmlAttrs!.lang,
+    class: 'font-sans',
   },
   link: [...(i18nHead.value.link ?? [])],
   meta: [...(i18nHead.value.meta ?? [])],
