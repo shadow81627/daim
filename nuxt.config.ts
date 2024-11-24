46db3e41db0a1acc880ee061a2437855848b730a
Damien Robinson <damien.robinson@daim.dev>
2024-11-24 12:37:44 +1000

    feat: add more web manifest icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -194,10 +194,25 @@ export default defineNuxtConfig({
       start_url: '/',
       icons: [
         {
-          src: 'icon.png',
+          src: 'pwa-64x64.png',
+          sizes: '64x64',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
           sizes: '512x512',
           type: 'image/png',
-          purpose: 'any',
+        },
+        {
+          src: 'maskable-icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
         },
       ],
     },
